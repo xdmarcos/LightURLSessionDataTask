57f90f721ea6210592adf704577951cab1a9664b
xdmarcos <xdmarcos@hotmail.com>
2021-04-14 16:09:40 +0100

    Remove MimeType file

--- Sources/LightURLSessionDataTask/Environment.swift
+++ Sources/LightURLSessionDataTask/Environment.swift
@@ -10,3 +10,16 @@ import Foundation
 public enum Environment {
   public static let successStatusCodeRange: Range<Int> = 200 ..< 300
 }
+
+public enum MimeType: String {
+  case json = "application/json"
+  case plain = "text/plain"
+  case javascript = "text/javascript"
+  case gif = "image/gif"
+  case png = "image/png"
+  case jpg = "image/jpg"
+  case jpeg = "image/jpeg"
+  case svg = "image/svg"
+  case multipart = "multipart/form-data"
+  case urlEncoded = "application/x-www-form-urlencoded"
+}
